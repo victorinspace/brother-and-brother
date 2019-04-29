50ab6b39012bb253632b9cb4faa07f761a513e36
Victor Velasquez <victorv24@gmail.com>
2019-04-29 15:34:02 -0700

    Added carousel back in home page with Houzz 100 Award taken out.

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,6 @@ import CarouselComponent from "../components/carousel/CarouselComponent";
 import oneYear from "../assets/images/awards/1year.png";
 import fiftyReviews from "../assets/images/awards/50reviews.png";
 import awardTwo from "../assets/images/awards/badge_47_8@2x.png";
-import awardThree from "../assets/images/awards/badge_29_9@2x.png";
 import awardFour from "../assets/images/awards/badge_44_8@2x.png";
 import awardFive from "../assets/images/awards/badge_47_8@2x.png";
 import homeAdvisor2017 from "../assets/images/awards/boha-2017.png";
@@ -31,13 +30,17 @@ import buildZoom from "../assets/images/awards/t10pl2.png";
 import topRated from "../assets/images/awards/toprated.png";
 import yelp from "../assets/images/awards/yelp.png";
 
-// import HomeVideo from "../images/mp4/BBSlide-Large.mov";
-// import ResponsiveEmbed from "react-bootstrap/ResponsiveEmbed";
-// import YouTube from "react-youtube";
-
 // import MainHero from "../assets/images/project-photos/BATHROOM-Timothy-Smith-Project/img_8986.jpg";
 
-// import ServicesHero from "../assets/images/project-photos/BATHROOM-lon-Minier/image-13.jpg";
+const VideoWrapper = styled.div`
+  @media (max-width: 800px) {
+    ${"" /* height: 100vh;
+    width: auto; */}
+    height: 0;
+    display: none;
+    background: 5px solid blue;
+  }
+`;
 
 const AwardsContainerContainer = styled.section`
   max-width: 90vw;
@@ -71,120 +74,22 @@ const Wrapper = styled.section`
   justify-content: center;
 `;
 
-// const VideoBackground = styled.div`
-//   background: #000;
-//   position: fixed;
-//   top: 0;
-//   bottom: 0;
-//   right: 0;
-//   left: 0;
-//   z-index: -99;
-//   &::after {
-//     display: block;
-//     content: "";
-//     position: absolute;
-//     top: 0;
-//     left: 0;
-//     width: 100%;
-//     height: 100%;
-//     z-index: 1;
-//     background: rgba(0, 0, 0, 0.75);
-//   }
-// `;
-
-// const VideoForeground = styled.div`
-//   position: absolute;
-//   top: 0;
-//   left: 0;
-//   width: 100%;
-//   height: 100%;
-//   pointer-events: none;
-
-//   @media (min-aspect-ratio: 16/9) {
-//     height: 300%;
-//     top: -100%;
-//   }
-
-//   @media (max-aspect-ratio: 16/9) {
-//     width: 300%;
-//     left: -100%;
-//   }
-// `;
-
-// _onReady(event) {
-//   // access to player in all event handlers via event.target
-//   // event.target.mute();
-// }
-
-// _onEnd(event) {
-//   event.target.playVideo();
-// }
-
-const videoOptions = {
-  playerVars: {
-    // https://developers.google.com/youtube/player_parameters
-    autoplay: 1,
-    controls: 0,
-    rel: 0,
-    showinfo: 0
-  }
-};
-
 const IndexPage = () => (
   <Layout>
     <div style={{ backgroundColor: `white` }}>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+
       {/* <Hero imgSrc={MainHero} pageTitle="Brother & Brother Builders" /> */}
-      {/* <CarouselComponent /> */}
 
-      <VideoComponent />
+      <VideoWrapper>
+        <VideoComponent className="video-component" />
+      </VideoWrapper>
 
-      {/* <div style={{ width: `660`, height: `auto` }}>
-      <ResponsiveEmbed aspect="a16by9">
-        <embed src="https://www.youtube-nocookie.com/embed/DOxVWYulV-s?controls=0" />
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube-nocookie.com/embed/DOxVWYulV-s?controls=0"
-          frameborder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        />
-      </ResponsiveEmbed>
-    </div> */}
-
-      {/* <VideoBackground>
-      <VideoForeground>
-        <YouTube
-          videoId="Z6FPJOgfCkc"
-          opts={autoplay: `1`,
-      controls: `0`,
-      rel: 0,
-      showinfo: 0}
-          className="video-iframe"
-          // onReady={this._onReady}
-          // onEnd={this._onEnd}
-        />
-      </VideoForeground>
-    </VideoBackground> */}
-
-      {/* <div
-      style={{
-        width: "300px",
-        height: "300px",
-        overflow: "hidden"
-      }}
-    >
-      <VideoCover videoOptions={{ src: `../images/mp4/BBSlide-Large.mp4` }} />
-    </div>
-    <Hero imgSrc={AboutHero} pageTitle="Who we are" subTitle="What we do best" /> */}
-      {/* <Hero imgSrc={HomeVideo} pageTitle="Who we are" subTitle="What we do best" /> */}
       <AwardsContainerContainer>
         <AwardsContainer>
           <AwardImage src={oneYear} alt="award" />
           <AwardImage src={fiftyReviews} alt="award" />
           <AwardImage src={awardTwo} alt="award" />
-          <AwardImage src={awardThree} alt="award" />
           <AwardImage src={awardFour} alt="award" />
           <AwardImage src={awardFive} alt="award" />
           <AwardImage src={homeAdvisor2017} alt="award" />
@@ -196,7 +101,7 @@ const IndexPage = () => (
           <AwardImage src={yelp} alt="award" />
         </AwardsContainer>
       </AwardsContainerContainer>
-      {/* <Hero imgSrc={ServicesHero} /> */}
+
       <Wrapper>
         <Container>
           <Row>
@@ -250,6 +155,7 @@ const IndexPage = () => (
           </Row>
         </Container>
       </Wrapper>
+      <CarouselComponent />
     </div>
   </Layout>
 );
