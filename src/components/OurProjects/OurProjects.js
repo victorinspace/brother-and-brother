f9282f4133b2c1a0d6fa3d3475765937fa25e89d
Victor Velasquez <victorv24@gmail.com>
2019-05-06 12:23:31 -0700

    Removed border in Projects component.

--- src/components/OurProjects/OurProjects.js
+++ src/components/OurProjects/OurProjects.js
@@ -14,7 +14,7 @@ const CarouselImg = styled.img`
 `;
 
 const OurProjects = () => (
-  <div style={{ border: `2px solid red`, paddingTop: `120px` }}>
+  <div style={{ paddingTop: `120px` }}>
     <h2>
       Our Projects {` `} <FaBriefcase />
     </h2>
