ae5ea944002742dfeda857a62de2dc0eb061f420
Victor Velasquez <victorv24@gmail.com>
2019-05-06 09:37:00 -0700

    Created carousel sliders for all categories in Services page.

--- src/pages/services.js
+++ src/pages/services.js
@@ -8,17 +8,32 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Carousel from "react-bootstrap/Carousel";
 
+// Hero Image
+import ServicesHero from "../assets/images/project-photos/BATHROOM-lon-Minier/image-13.jpg";
+
 // Full Remodeling Carousl Items
 import FullOne from "../assets/images/project-photos/FULL-The-Silkwood-Project/K1_12863.jpg";
 import FullTwo from "../assets/images/project-photos/FULL-The-Silkwood-Project/K1_12980.jpg";
 import FullThree from "../assets/images/project-photos/FULL-The-Silkwood-Project/K1_13034.jpg";
 import FullFour from "../assets/images/project-photos/FULL-The-Silkwood-Project/K1_12958.jpg";
 
-import RoomAddition from "../assets/images/project-photos/ADDITION-The-Cedar-Project/K1_14763.jpg";
-import Kitchens from "../assets/images/project-photos/KITCHEN-BATHROOM-The-Nelson-Project/K1_19487.jpg";
-import Bathroom from "../assets/images/project-photos/BATHROOM-Castro-Valley/1Y6A1829.jpg";
+// Additions Carousel Items
+import AdditionsOne from "../assets/images/project-photos/ADDITION-The-Cedar-Project/K1_14712.jpg";
+import AdditionsTwo from "../assets/images/project-photos/ADDITION-The-Cedar-Project/K1_14763.jpg";
+import AdditionsThree from "../assets/images/project-photos/ADDITION-The-Cedar-Project/K1_14747.jpg";
+import AdditionsFour from "../assets/images/project-photos/ADDITION-The-Cedar-Project/K1_14722-HDR.jpg";
 
-import ServicesHero from "../assets/images/project-photos/BATHROOM-lon-Minier/image-13.jpg";
+// Kitchens Carousel Items
+import KitchensOne from "../assets/images/project-photos/KITCHEN-BATHROOM-The-Nelson-Project/K1_19487.jpg";
+import KitchensTwo from "../assets/images/project-photos/KITCHEN-The-Arezzo-Project/K1_15428-1.jpg";
+import KitchensThree from "../assets/images/project-photos/KITCHEN-The-Arezzo-Project/K1_15511-2.jpg";
+import KitchensFour from "../assets/images/project-photos/Kitchen-&-Bathrooms_The-Castano-Project/K1_19593.jpg";
+
+// Bathroom Carousel Items
+import BathroomOne from "../assets/images/project-photos/BATHROOM-The-Beaumont-Project/SF_BTH-10 copy.jpg";
+import BathroomTwo from "../assets/images/project-photos/BATHROOM-The-Arezzo-Project/K1_14680.jpg";
+import BathroomThree from "../assets/images/project-photos/BATHROOM-Castro-Valley/1Y6A1829.jpg";
+import BathroomFour from "../assets/images/project-photos/BATHROOM-The-Gentrytown-Project/IMG_92960-sm.jpg";
 
 const Wrapper = styled.section`
   max-width: 1000px;
@@ -68,7 +83,20 @@ const Services = () => (
           </Card>
 
           <Card border="light" style={{ width: `25.5rem`, margin: `0.45rem` }}>
-            <Card.Img variant="top" src={RoomAddition} alt="Room Additions" />
+            <Carousel>
+              <Carousel.Item>
+                <img src={AdditionsOne} alt="room additions" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={AdditionsTwo} alt="room additions" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={AdditionsThree} alt="room additions" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={AdditionsFour} alt="room additions" />
+              </Carousel.Item>
+            </Carousel>
             <Card.Body>
               <Card.Title>Room Additions</Card.Title>
               <Card.Text>
@@ -81,7 +109,20 @@ const Services = () => (
 
         <Row>
           <Card border="light" style={{ width: `25.5rem`, margin: `0.45rem` }}>
-            <Card.Img variant="top" src={Kitchens} alt="Kitchens" />
+            <Carousel>
+              <Carousel.Item>
+                <img src={KitchensOne} alt="kitchens" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={KitchensTwo} alt="kitchens" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={KitchensThree} alt="kitchens" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={KitchensFour} alt="kitchens" />
+              </Carousel.Item>
+            </Carousel>
             <Card.Body>
               <Card.Title>Kitchens</Card.Title>
               <Card.Text>
@@ -93,7 +134,20 @@ const Services = () => (
           </Card>
 
           <Card border="light" style={{ width: `25.5rem`, margin: `0.45rem` }}>
-            <Card.Img variant="top" src={Bathroom} alt="Bathroom" />
+            <Carousel>
+              <Carousel.Item>
+                <img src={BathroomOne} alt="bathrooms" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={BathroomTwo} alt="bathrooms" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={BathroomThree} alt="bathrooms" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={BathroomFour} alt="bathrooms" />
+              </Carousel.Item>
+            </Carousel>
             <Card.Body>
               <Card.Title>Bathroom</Card.Title>
               <Card.Text>
