649f17d9a4108530d93011c2a570307735765f7e
Victor Velasquez <victorv24@gmail.com>
2019-05-06 12:39:41 -0700

    Removed Our Awards files.

--- src/components/OurAwards/OurAwards.js
+++ src/components/OurAwards/OurAwards.js
@@ -1,61 +1,80 @@
 import React from "react";
 import styled from "styled-components";
-import { FaAward } from "react-icons/fa";
+import { MdStar, MdStarHalf } from "react-icons/md";
 
-import oneYear from "../../assets/images/awards/1year.png";
-import fiftyReviews from "../../assets/images/awards/50reviews.png";
-import awardTwo from "../../assets/images/awards/badge_47_8@2x.png";
-import awardFour from "../../assets/images/awards/badge_44_8@2x.png";
-import awardFive from "../../assets/images/awards/badge_47_8@2x.png";
-import homeAdvisor2017 from "../../assets/images/awards/boha-2017.png";
-import homeAdvisor2018 from "../../assets/images/awards/boha-2018.png";
-import elite from "../../assets/images/awards/elite.png";
-import buildZoom from "../../assets/images/awards/t10pl2.png";
-import topRated from "../../assets/images/awards/toprated.png";
-import yelp from "../../assets/images/awards/yelp.png";
+import FacebookIcon from "../../assets/images/BB-Review-Images/fb.png";
+import HomeAdvisorIcon from "../../assets/images/BB-Review-Images/ha.png";
+import HouzzIcon from "../../assets/images/BB-Review-Images/hz.png";
+import YelpIcon from "../../assets/images/BB-Review-Images/yelp.png";
 
-const AwardsContainerContainer = styled.section`
-  max-width: 800px;
+const ReviewIconsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  max-width: 80%;
   margin: 0 auto;
-  padding: 3rem 0;
-  display: block;
+  padding: 0 0 5rem;
 `;
 
-const AwardsContainer = styled.section`
+const ReviewIconsBox = styled.div`
+  ${"" /* padding: 0.3rem; */}
+  max-width: 25%;
+  ${"" /* margin: 0.5rem; */}
   margin: 0 auto;
-  padding: 1rem 0;
+  ${"" /* border: 1px solid red; */}
+`;
+
+const ReviewStarsContainer = styled.div`
+  font-size: 1.3rem;
+  color: #9caa49;
   display: flex;
   flex-direction: row;
-  align-items: center;
   flex-wrap: wrap;
-  justify-content: space-between;
+  ${"" /* border: 1px solid blue; */}
 `;
 
-const AwardImage = styled.img`
-  max-width: 6rem;
-  padding: 0.66rem;
-`;
-
-const OurAwards = () => (
-  <AwardsContainerContainer>
-    <h2 style={{ margin: `0 10%` }}>
-      Awards and Recognition {` `} <FaAward />
-    </h2>
-    <AwardsContainer>
-      <AwardImage src={oneYear} alt="award" />
-      <AwardImage src={fiftyReviews} alt="award" />
-      <AwardImage src={awardTwo} alt="award" />
-      <AwardImage src={awardFour} alt="award" />
-      <AwardImage src={awardFive} alt="award" />
-      <AwardImage src={homeAdvisor2017} alt="award" />
-      <AwardImage src={homeAdvisor2018} alt="award" />
-      <AwardImage src={elite} alt="award" />
-      <AwardImage src={buildZoom} alt="award" />
-      <AwardImage src={topRated} alt="award" />
-      <AwardImage src={elite} alt="award" />
-      <AwardImage src={yelp} alt="award" />
-    </AwardsContainer>
-  </AwardsContainerContainer>
+const OurReviews = () => (
+  <ReviewIconsContainer>
+    <ReviewIconsBox>
+      <img src={FacebookIcon} alt="Facebook" />
+      <ReviewStarsContainer>
+        <MdStar />
+        <MdStar />
+        <MdStar />
+        <MdStar />
+        <MdStar />
+      </ReviewStarsContainer>
+    </ReviewIconsBox>
+    <ReviewIconsBox>
+      <img src={YelpIcon} alt="Yelp" />
+      <ReviewStarsContainer>
+        <MdStar />
+        <MdStar />
+        <MdStar />
+        <MdStar />
+        <MdStarHalf />
+      </ReviewStarsContainer>
+    </ReviewIconsBox>
+    <ReviewIconsBox>
+      <img src={HouzzIcon} alt="Houzz" />
+      <ReviewStarsContainer>
+        <MdStar />
+        <MdStar />
+        <MdStar />
+        <MdStar />
+        <MdStar />
+      </ReviewStarsContainer>
+    </ReviewIconsBox>
+    <ReviewIconsBox>
+      <img src={HomeAdvisorIcon} alt="Home Advisor" />
+      <ReviewStarsContainer>
+        <MdStar />
+        <MdStar />
+        <MdStar />
+        <MdStar />
+        <MdStarHalf />
+      </ReviewStarsContainer>
+    </ReviewIconsBox>
+  </ReviewIconsContainer>
 );
 
-export default OurAwards;
+export default OurReviews;
